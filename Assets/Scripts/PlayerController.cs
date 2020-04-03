cef8f6a30d8e471d19ea53250afc7e9f5cf94299
Jeremy <jeremy.tapia30@hotmail.com>
2020-04-03 20:33:46 +0200

    Dossiers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    #region Components
+    Rigidbody2D rb2D;
+    #endregion
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
